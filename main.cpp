85d4f7b38232b6571a554b43ea8ef7448ea87237
Hieu_Truong <minhhieutruong226@gmail.com>
2023-07-06 19:13:36 +0700

    Insert node using recursion

--- main.cpp
+++ main.cpp
@@ -12,11 +12,10 @@ class Node{
 };
 
 class BST{
-    private:
-        Node* root = NULL;
     public:
         BST(){}
-        
+        Node* root = NULL;
+
         void insertNode(int data){
             Node* newNode = new Node;
             newNode->data = data;
@@ -49,20 +48,56 @@ class BST{
             }
             
         }
+
+        Node* insertNodeRecur(Node* root,int data){
+            Node* newNode = new Node;
+            newNode->data = data;
+            newNode->left = NULL;
+            newNode->right = NULL;
+
+            if(root==NULL){
+                root = newNode;
+            }else{
+                if(data > root->data){
+
+                    if(root->right==NULL){
+                        root->right = newNode;
+                    }else{
+                        delete newNode;
+                        insertNodeRecur(root->right,data);
+                    }
+                    
+                }else{
+
+                    if(root->left==NULL){
+                        root->left = newNode;
+                    }else{
+                        delete newNode;
+                        insertNodeRecur(root->left,data);
+                    }
+
+                }
+            }
+            return root;
+        }
 };
 
 int main()
 {
     BST bst;
-    bst.insertNode(8);
-    bst.insertNode(3);
-    bst.insertNode(10);  
-    bst.insertNode(1);  
-    bst.insertNode(6);
-    bst.insertNode(14);  
-    bst.insertNode(4);
-    bst.insertNode(7);  
-    bst.insertNode(13);
+    int arr[9] = {8,3,10,1,6,14,4,7,13};
+    for(int i = 0; i < 8; i++){
+        bst.root = bst.insertNodeRecur(bst.root,arr[i]);
+    }
+    // bst.root = bst.insertNodeRecur(bst.root,8);
+    // bst.root = bst.insertNodeRecur(bst.root,3);
+    // bst.root = bst.insertNodeRecur(bst.root,10);  
+    // bst.root = bst.insertNodeRecur(bst.root,1);  
+    // bst.root = bst.insertNodeRecur(bst.root,6);
+    // bst.root = bst.insertNodeRecur(bst.root,14);  
+    // bst.root = bst.insertNodeRecur(bst.root,4);
+    // bst.root = bst.insertNodeRecur(bst.root,7);  
+    // bst.root = bst.insertNodeRecur(bst.root,13);
     // Node* firstNode = new Node;
     // Node* secondNode = new Node;
     // Node* thridNode = new Node;
